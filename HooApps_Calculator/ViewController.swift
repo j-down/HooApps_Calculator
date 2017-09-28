f1b2f67b52af20a85b434e88ecf436676ae82dbc
Admin <jareddowning@Jareds-MacBook-Pro.local>
2017-09-28 00:35:18 -0400

    update from 9.27.2017

--- HooApps_Calculator/ViewController.swift
+++ HooApps_Calculator/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var topLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    @IBAction func onePressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        setup()
+    }
+    
+    func setup() {
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,115 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func append(from sender: Any) {
+        
+        //Cleaner
+        if let button = sender as? UIButton,
+           let text = button.titleLabel?.text,
+           let topText = topLabel?.text{
+            
+                if topText == "0" {
+                    
+                    topLabel.text = text
+                }
+                else {
+                    
+                    topLabel.text = topText + text
+                }
+            
+        }
+    }
 
+    
+    @IBAction func twoPressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    
+    @IBAction func threePressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    @IBAction func fourPressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    @IBAction func fivePressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    @IBAction func sixPressed(_ sender: Any) {
+        
+        
+        append(from: sender)
+    }
+    
+    @IBAction func sevenPressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    @IBAction func eightPressed(_ sender: Any) {
+        
+        append(from: sender)
+        
+    }
+    
+    @IBAction func ninePressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    
+    @IBAction func zeroPressed(_ sender: Any) {
+        
+        append(from: sender)
+    }
+    
+    @IBAction func plusPressed(_ sender: Any) {
+        
+        add()
+    }
+    
+    
+    @IBAction func minusPressed(_ sender: Any) {
+        
+        subtract()
+        
+    }
+    
+    func add() {
+        
+        if let text = topLabel.text{
+            
+            let intText = Int(text)
+            
+            
+            
+        }
+    }
+    
+    func subtract() {
+        
+        
+    }
+    
+    
+    @IBAction func equalPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
